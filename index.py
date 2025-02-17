32b45e1db581b3b91d0369c99c8b04f957b5a700
saujan <sauj0ndahal@gmail.com>
2025-02-17 13:43:02 +0545

    new commit

--- index.py
+++ index.py
@@ -1,1 +1,66 @@
-print("new line of VS code")
+""" print ("new line of VS code")
+print(8 * "\n")
+print ("new line of VS code") """
+
+""" print ("Welcome to ", end="") 
+print ("Guru99", end = '!') """
+
+""" DataTypes """
+
+""" a = 100
+print(a)
+a="new"
+print(a)
+
+a="sam"
+
+b=0
+print (a+str(b)) """
+
+
+""" f= 10000
+print (f)
+
+def someFunction():
+f = 'this is text'
+print(f)
+
+someFunction()
+
+print(f) """
+
+
+""" f = 101
+print(f) 
+
+def someFunction():
+   
+    f = 'I am learning Python' 
+    print(f) 
+    
+someFunction()
+
+a="i am boared"
+print(a)  
+someFunction()
+
+print(f)  """
+
+
+
+f = 101
+print(f) 
+
+def someFunction():
+   
+    f = 'I am learning Python' 
+    print(f) 
+    
+    a="i am boared"
+    print(a)
+someFunction()
+
+ 
+someFunction()
+
+print(f)
